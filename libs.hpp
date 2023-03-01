595aec9b2244cc0b82c5e61c468507a02cf175c7
Abderrahim Zabir <bahimzabir@gmail.com>
2023-03-01 19:49:20 +0100

    start parsing the config file

---
+++ libs.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libs.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azabir <azabir@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 16:26:19 by azabir            #+#    #+#             */
+/*   Updated: 2023/03/01 16:26:20 by azabir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBS_HPP
+#define LIBS_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <string>
+#include <fstream>
+
+
+
+#endif
