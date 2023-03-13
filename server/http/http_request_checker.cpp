ea8a3ad33682f8cbb5ed0c7c36ac45eb69262cd9
Abderrahim Zabir <bahimzabir@gmail.com>
2023-03-13 22:58:17 +0100

    http requirements defined

--- server/http/http_request_checker.cpp
+++ server/http/http_request_checker.cpp
@@ -10,3 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "http_request.hpp"
+
+/*
+TO DO:
+the mandatory requirements for each HTTP request:
+
+ALL requests:
+	The request must contain a valid resource path in the form of an absolute or relative URL.
+	The request must contain a valid HTTP version, which must be either HTTP/1.0 or HTTP/1.1.
+GET & DELETE requests:
+	The method must be GET || DELETE.
+POST request:
+	The method must be POST.
+	The request must include a valid Content-Length header specifying the length of the message body in bytes.
+	The request must include a Content-Type header specifying the type of data being sent.
+	The message body must contain the data being submitted to the server in the correct format specified by the Content-Type header.
+*/
+
+
